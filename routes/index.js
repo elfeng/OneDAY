0110d775c0b5145a32916ca9d1163d2ca4c45ac8
Elaine Feng <eleanor52@live.ca>
2017-03-15 02:44:04 -0700

    Added connection to db via mongoose, set up first collection model

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/one-day');
+
+var userSchema = {
+    username: String,
+    password: String,
+    email: String,
+    partner: String,
+    preferences: Array,
+    activitiesAddedToCalendar: Array
+}
+
+var Users = mongoose.model('Users', userSchema, 'users');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    console.log(req);
+    res.render('index', { title: 'One DAY', author: 'Elaine' });
 });
 
 module.exports = router;
