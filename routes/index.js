bc01000d6cb9ff187b91ba3351695113399ccfe0
Elaine Feng <eleanor52@live.ca>
2017-03-16 21:57:28 -0700

    Implemented recommendations and preferences routers, added both views

--- routes/index.js
+++ routes/index.js
@@ -20,11 +20,29 @@ router.get('/', function(req, res, next) {
     res.render('index', {user: {}, title: 'One DAY'});
 });
 
+/* GET user object and insert it into preference form */
+router.get('/users/:id', function(req,res, next) {
+    Users.findById(req.params.id).exec(function(err, doc) {
+        res.render('preferences', {user: doc, action: '/users/' + doc._id + '/update'});
+    });
+});
+
+/* POST user object to current object in database */
+router.post('/users/:id/update', function(req, res, next) {
+   Users.update({_id: req.params.id}, {$set: req.body}).exec(function(err, doc) {
+        if (err) {
+
+        } else {
+            res.redirect('/users/' + req.params.id);
+        }
+    });
+});
+
 /* POST add user and direct to recommendations page upon login */
 router.post('/users/:id/recommendations', function(req, res, next) {
     var newUser = new Users(req.body);
     newUser.save(function (err, doc) {
-        res.render('recommendations', {title: 'Recommendations', user: doc});
+        res.render('recommendations', {user: doc});
     });
 });
 
