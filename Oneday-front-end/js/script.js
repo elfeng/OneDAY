5799d41394f5e4c1a930c18a3435457f42b6b83c
apple <apple@appledeMacBook-Air-3.local>
2017-03-17 01:08:47 -0700

    update home page and sign-in

---
+++ Oneday-front-end/js/script.js
@@ -0,0 +1,16 @@
+
+
+//  var myLatlng = new google.maps.LatLng(49.246292
+// ,-123.116);
+// var mapOptions = {
+//   zoom: 4,
+//   center: myLatlng
+// }
+// var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+// var marker = new google.maps.Marker({
+//     position: myLatlng,
+//     title:"Hello World!"
+// });
+
+// marker.setMap(map);
