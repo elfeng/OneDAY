ea75dbb29802fc4556de250a850e14fba82a1f7f
Elaine Feng <eleanor52@live.ca>
2017-03-17 01:51:18 -0700

    Refactored route paths, implemented router and view for footprints page

--- routes/index.js
+++ routes/index.js
@@ -15,34 +15,40 @@ var userSchema = {
 
 var Users = mongoose.model('Users', userSchema, 'users');
 
-/* GET login page. */
+/* GET home page */
+// TODO: move login form to separate page
 router.get('/', function(req, res, next) {
     res.render('index', {user: {}, title: 'One DAY'});
 });
 
+/* POST add user and direct to recommendations page upon login */
+router.post('/:id/recommendations', function(req, res, next) {
+    var newUser = new Users(req.body);
+    newUser.save(function (err, doc) {
+        res.render('recommendations', {user: doc, partner: doc.partner, title: 'One DAY'});
+    });
+});
+
+/* GET footprints page */
+router.get('/:id/footprints', function(req, res, next) {
+    res.render('footprints', {user: {}, title: 'One DAY'});
+});
+
 /* GET user object and insert it into preference form */
-router.get('/users/:id', function(req,res, next) {
+router.get('/:id', function(req,res, next) {
     Users.findById(req.params.id).exec(function(err, doc) {
-        res.render('preferences', {user: doc, action: '/users/' + doc._id + '/update'});
+        res.render('preferences', {user: doc, action: '/' + doc._id, title: 'One DAY'});
     });
 });
 
 /* POST user object to current object in database */
-router.post('/users/:id/update', function(req, res, next) {
-   Users.update({_id: req.params.id}, {$set: req.body}).exec(function(err, doc) {
+router.post('/:id', function(req, res, next) {
+    Users.update({_id: req.params.id}, {$set: req.body}).exec(function(err, doc) {
         if (err) {
 
         } else {
-            res.redirect('/users/' + req.params.id);
+            res.redirect(req.params.id);
         }
-    });
-});
-
-/* POST add user and direct to recommendations page upon login */
-router.post('/users/:id/recommendations', function(req, res, next) {
-    var newUser = new Users(req.body);
-    newUser.save(function (err, doc) {
-        res.render('recommendations', {user: doc});
     });
 });
 
