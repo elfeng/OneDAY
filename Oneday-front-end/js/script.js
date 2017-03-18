ff4d73979f1de87dd11a7840153e0d1fcad94b75
apple <apple@appledeMacBook-Air-3.local>
2017-03-18 00:25:43 -0700

    update home page and preference

--- Oneday-front-end/js/script.js
+++ Oneday-front-end/js/script.js
@@ -1,16 +1,87 @@
+var token;
+var markers[];
+var markersMap{};
+function getCookie(name) {   // helper function 
+	var value = "; " + document.cookie;
+	var parts = value.split("; " + name + "=");
+	if(parts.length == 2) return parts.pop().split(";").shift();
+}
 
+$(document).ready(function(event) { //garantee HTML 已经ready
+$.ajax({
+		url:"http://open-commerce.herokuapp.com/api/products",
+		type:"GET",
+		data:{
+			'token':token
+		},
+		success: function(res) {
+ 			markers = res;
+ 			    
+ 					},
+		error: function(err){
+		}
+	});
 
-//  var myLatlng = new google.maps.LatLng(49.246292
-// ,-123.116);
-// var mapOptions = {
-//   zoom: 4,
-//   center: myLatlng
-// }
-// var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+    token = getCookie('x-access-token');
+    getProducts();
+    updateView();
+    $("#logoutNav").click(function(event) {
+    	event.preventDefault();
+    	deleteCookie('x-access-token');
+    	deleteCookie('cart');
+    	token = null; 
+    	updateView();
+    	window.location.href = "about.html";
+    })
 
-// var marker = new google.maps.Marker({
-//     position: myLatlng,
-//     title:"Hello World!"
-// });
+    $("#loginBtn").click(function(event) {
+            event.preventDefault();
+            alert("hello ");
+            var username = $("#username").val();
+            var password = $("#password").val();
+            if(username && password) {
+            	$.post("http://open-commerce.herokuapp.com/api/login",
+            	{
+            		username: username,
+                	password: password	
+            	},
+            	function(res) {
+            		if(res.success) {
+            			alert("login success");
+            			var cookie = 'x-access-token=' + res.token; 
+            			document.cookie = cookie;
+            			window.location.href = "about.html";
+            			} else {
+            			alert(res.message);
+            			}
+            		}
+				)}
+             else {
+            	alert("Please provide a username and password");
+            }
+    
+    $("#signupBtn").click(function(event) {
+            // console.log(event);
+            event.preventDefault();
+            var username = $("#username").val();
+            var password = $("#password").val();
+            if (username && password) {
+                $.post(" http://open-commerce.herokuapp.com/api/signup", 
+                	{
+                        username: username,
+                        password: password
+                    },
+                    function(res) {
+                        if (res.success) {
+                            alert("signup success");
+                        } else {
+                            alert(res.message);
+                        }
+                    }
+                )} else {
+            		alert("Enter username and password");
+        		}
 
-// marker.setMap(map);
+    });
+});
+})
